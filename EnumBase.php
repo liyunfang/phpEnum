503891ea9ef0e2985832261d5db66df81e5d527f
mydream <381296986@qq.com>
2015-01-23 14:42:39 +0800

    Create EnumBase.php
    add Enumeration base

---
+++ EnumBase.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of EnumBase
+ *
+ * @author liyunfang <381296986@qq.com>
+ * @date 2015-01-23
+ */
+class EnumBase {
+    //protected static $names = array();
+    /**
+     * 验证合法性
+     * @param type $value
+     * @return type
+     */
+    public static function validate($value)
+    {
+        $childClass  =  new static();
+        $names = $childClass::$names;
+        return isset($names[$value]);
+    }
+
+    /**
+     * 获取枚举描述
+     * @param type $value
+     * @return string
+     */
+    public static function getName($value)
+    {
+        if(!empty($value))
+        {
+            $childClass  =  new static();
+            $names = $childClass::$names;
+            if(isset($names[$value]))
+            {
+                return $names[$value];
+            }
+        }
+        return '未知';
+    }
+}
